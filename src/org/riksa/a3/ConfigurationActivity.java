8ab61177ad27922af8926ff5017be4daac20a02e
riku salkia <riksa@iki.fi>
2012-10-17 18:08:29 +0300

    initial import

---
+++ src/org/riksa/a3/ConfigurationActivity.java
@@ -0,0 +1,15 @@
+package org.riksa.a3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigurationActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
