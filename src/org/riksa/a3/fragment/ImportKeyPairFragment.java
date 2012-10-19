07f5178d3c082552ce93654576bc3a5bcd207d63
riku salkia <riksa@iki.fi>
2012-10-19 22:27:32 +0300

    kind of validation for keys

--- src/org/riksa/a3/fragment/ImportKeyPairFragment.java
+++ src/org/riksa/a3/fragment/ImportKeyPairFragment.java
@@ -9,10 +9,15 @@ package org.riksa.a3.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import org.riksa.a3.R;
 import org.riksa.a3.exception.ViewNotFoundException;
 import org.riksa.a3.util.A3Utils;
@@ -49,9 +54,90 @@ public class ImportKeyPairFragment extends Fragment {
                     onBrowsePublicClicked(view);
                 }
             });
+
+            EditText privateKeyEditText = A3Utils.findView(getActivity(), EditText.class, R.id.pk_private_key_path);
+            privateKeyEditText.addTextChangedListener(new TextWatcher() {
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    validateKeys();
+                }
+
+                public void afterTextChanged(Editable editable) {
+                    //To change body of implemented methods use File | Settings | File Templates.
+                }
+            });
+
+            EditText publicKeyEditText = A3Utils.findView(getActivity(), EditText.class, R.id.pk_public_key_path);
+            publicKeyEditText.addTextChangedListener(new TextWatcher() {
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    //To change body of implemented methods use File | Settings | File Templates.
+                }
+
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    validateKeys();
+                }
+
+                public void afterTextChanged(Editable editable) {
+                    //To change body of implemented methods use File | Settings | File Templates.
+                }
+            });
+
         } catch (ViewNotFoundException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    void validateKeys() {
+        try {
+            Button importButton = A3Utils.findView(getActivity(), Button.class, R.id.pk_btn_import);
+            importButton.setEnabled(false);
+            EditText privateEdit = A3Utils.findView(getActivity(), EditText.class, R.id.pk_private_key_path);
+            EditText publicEdit = A3Utils.findView(getActivity(), EditText.class, R.id.pk_public_key_path);
+            if (validatePrivateKey(privateEdit.getText().toString()) && validatePublicKey(publicEdit.getText().toString())) {
+                importButton.setEnabled(true);
+            }
+        } catch (ViewNotFoundException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private boolean validatePrivateKey(String privateKey) throws ViewNotFoundException {
+        ImageView imageView = A3Utils.findView(getActivity(), ImageView.class, R.id.pk_icon_private_key_valid);
+        TextView textView = A3Utils.findView(getActivity(), TextView.class, R.id.pk_text_private_key_valid);
+        textView.setText(R.string.pk_text_private_key_invalid);
+        imageView.setImageResource(R.drawable.btn_check_buttonless_off);
+        if (privateKey == null || privateKey.length() == 0) {
+            return false;
+        }
+
+        log.debug("privateKey={}", privateKey);
+
+        String type = "RSA";
+        int bits = 1024;
+        textView.setText(getString(R.string.pk_text_private_key_valid, type, bits));
+        imageView.setImageResource(R.drawable.btn_check_buttonless_on);
+        return true;
+    }
+
+    private boolean validatePublicKey(String publicKey) throws ViewNotFoundException {
+        ImageView imageView = A3Utils.findView(getActivity(), ImageView.class, R.id.pk_icon_public_key_valid);
+        TextView textView = A3Utils.findView(getActivity(), TextView.class, R.id.pk_text_public_key_valid);
+        textView.setText(R.string.pk_text_public_key_invalid);
+        imageView.setImageResource(R.drawable.btn_check_buttonless_off);
+        if (publicKey == null || publicKey.length() == 0) {
+            return false;
+        }
+
+        log.debug("publicKey={}", publicKey);
+
+        String type = "RSA";
+        int bits = 1024;
+        textView.setText(getString(R.string.pk_text_public_key_valid, type, bits));
+        imageView.setImageResource(R.drawable.btn_check_buttonless_on);
+        return true;
     }
 
     private void onBrowsePrivateClicked(View view) {
